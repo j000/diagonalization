5a9ca22312b4d47c5266460a4717940ddbbad70b
j000 <jarymut+github@gmail.com>
2017-11-30 14:14:40 +0100

    Start implementation

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <math.h>
+
+#include <sys/random.h>
+
+#include <mkl_solvers_ee.h>
+#include <mkl_vsl.h>
+#include <mkl.h>
+
+int main(int argc, char **argv) {
+}
